c9b7a699cba397f0b7e90ab93fcd80d07d16ba9a
collin <collin0825@gmail.com>
2023-04-29 16:22:10 +0800

    kg bug fix

--- brain/config.py
+++ brain/config.py
@@ -7,6 +7,7 @@ KGS = {
     'HowNet': os.path.join(FILE_DIR_PATH, 'kgs/HowNet.spo'),
     'CnDbpedia': os.path.join(FILE_DIR_PATH, 'kgs/CnDbpedia.spo'),
     'Medical': os.path.join(FILE_DIR_PATH, 'kgs/Medical.spo'),
+    'primeKG': os.path.join(FILE_DIR_PATH, 'kgs/primeKG.spo'),
 }
 
 MAX_ENTITIES = 2
